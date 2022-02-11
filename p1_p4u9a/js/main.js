2cb4f3d96fdd5a083eedd29cff653138a52dbe3f
Kevin <kevincgc@gmail.com>
2022-02-11 14:30:32 -0800

    mostly working, some row labels are not disappearing

--- p1_p4u9a/js/main.js
+++ p1_p4u9a/js/main.js
@@ -1,37 +1,45 @@
 // Initialize helper function to convert date strings to date objects
 const parseTime = d3.timeParse("%Y-%m-%d");
+const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const disasterCategories = ["winter-storm-freeze", "flooding", "severe-storm", "drought-wildfire", "tropical-cyclone"];
+let data, timeline;
 
 //Load data from CSV file asynchronously and render chart
-d3.csv('data/disaster_costs.csv').then(data => {
+d3.csv('data/disaster_costs.csv').then(_data => {
+  data = _data;
   data.forEach(d => {
     d.cost = +d.cost;
     d.year = +d.year;
     d.date = parseTime(d.mid);
-    let yearStart = new Date(d.year, 1, 1);
+
+    let yearStart = new Date(d.year, 0, 0);
     d.dayOfYear = d3.timeDay.count(yearStart, d.date);
-    d.month = d.date.getMonth();
+    d.month = monthLabels[d.date.getMonth()];
     // Optional: other data preprocessing steps
   });
   console.log(data);
-  const timeline = new Timeline({
+  timeline = new Timeline({
     parentElement: '#vis',
-    disasterCategories: ["winter-storm-freeze", "flooding", "severe-storm", "drought-wildfire", "tropical-cyclone"],
+    disasterCategories: disasterCategories,
+    monthLabels: monthLabels,
     // Optional: other configurations
   }, data);
 
 }).catch(error => console.error(error));
 
-d3.selectAll('.legend-btn').on('click', function() {
+d3.selectAll('.legend-label').on('click', function() {
   // Toggle 'inactive' class
   d3.select(this).classed('inactive', !d3.select(this).classed('inactive'));
 
   // Check which categories are active
-  let selectedDifficulty = [];
-  d3.selectAll('.legend-btn:not(.inactive)').each(function() {
-    selectedDifficulty.push(d3.select(this).attr('data-difficulty'));
+  timeline.selectedCategories = [];
+  d3.selectAll('.legend-label:not(.inactive)').each(function() {
+    timeline.selectedCategories.push(d3.select(this).attr('data-category'));
   });
-
-  // Filter data accordingly and update vis
-  scatterplot.data = data.filter(d => selectedDifficulty.includes(d.difficulty));
-  scatterplot.updateVis();
+  if (timeline.selectedCategories.length === 0) {
+    timeline.selectedCategories = disasterCategories;
+  }
+  console.log(timeline.selectedCategories);
+  timeline.data = data.filter(d => timeline.selectedCategories.includes(d.category));
+  timeline.updateVis();
 });
--- p1_p4u9a/js/timeline.js
+++ p1_p4u9a/js/timeline.js
@@ -1,199 +1,213 @@
 class Timeline {
 
-  /**
-   * Class constructor with initial configuration
-   * @param {Object}
-   * @param {Array}
-   */
-  constructor(_config, _data) {
-    this.config = {
-      parentElement: _config.parentElement,
-      disasterCategories: _config.disasterCategories,
-      containerWidth: 800,
-      containerHeight: 900,
-      tooltipPadding: 15,
-      margin: {top: 120, right: 20, bottom: 20, left: 45},
-      legendWidth: 170,
-      legendHeight: 8,
-      legendRadius: 5
+    /**
+     * Class constructor with initial configuration
+     * @param {Object}
+     * @param {Array}
+     */
+    constructor(_config, _data) {
+        this.config = {
+            parentElement: _config.parentElement,
+            disasterCategories: _config.disasterCategories,
+            monthLabels: _config.monthLabels,
+            containerWidth: 800,
+            containerHeight: 900,
+            tooltipPadding: 15,
+            margin: {top: 120, right: 20, bottom: 20, left: 45},
+            legendWidth: 170,
+            legendHeight: 8,
+            legendRadius: 5
+        }
+        this.colors = ["#ccc", "#41b6c4", "#c7e9b4", "#ffffd9", "#081d58"];
+        this.data = _data;
+        this.selectedCategories = [];
+        this.initVis();
     }
-    this.monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    this.data = _data;
-    this.selectedCategories = [];
-    this.initVis();
-  }
-  
-  /**
-   * We initialize the arc generator, scales, axes, and append static elements
-   */
-  initVis() {
-    let vis = this;
 
-    // Calculate inner chart size. Margin specifies the space around the actual chart.
-    vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-    vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+    /**
+     * We initialize the arc generator, scales, axes, and append static elements
+     */
+    initVis() {
+        let vis = this;
 
-    // Todo: Initialize scales and axes
-    //===================BEGIN=====================================================================================================================
-    // Initialize scales
-    vis.radiusScale = d3.scaleSqrt()
-        .range([4, 140]);
+        // Calculate inner chart size. Margin specifies the space around the actual chart.
+        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
+        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
 
-    vis.xScale = d3.scaleLinear()
-        .range([0, vis.width]);
+        // Todo: Initialize scales and axes
+        //===================BEGIN=====================================================================================================================
+        // Initialize scales
+        vis.radiusValue = d => d.cost;
+        vis.radiusScale = d3.scaleSqrt()
+            .domain(d3.extent(vis.data, vis.radiusValue))
+            .range([4, 140]);
 
-    vis.yScale = d3.scaleBand()
-        .range([0, vis.config.containerHeight])
-        .paddingInner(0.2);
+        vis.xScale = d3.scaleLinear()
+            .domain([0, 365])
+            .range([0, vis.width]);
 
-    vis.topScale = d3.scaleLinear()
-        .range([0, vis.config.width]);
+        vis.yScale = d3.scaleLinear()
+            .domain([2017, 1980])
+            .range([0, vis.config.containerHeight - 150]);
 
-    // Initialize axes
-    vis.xAxis = d3.axisTop(vis.xScale)
-        .ticks(11)
-        .tickSize(0)
-        .tickFormat(d3.format('d')) // Remove comma delimiter for thousands
-        .tickPadding(10);
+        vis.monthScale = d3.scaleBand()
+            .domain(vis.config.monthLabels)
+            .range([0, vis.width]);
 
-    vis.yAxis = d3.axisLeft(vis.yScale).tickSize(-vis.width);
+        vis.colorScale = d3.scaleOrdinal()
+            .domain(vis.config.disasterCategories)
+            .range(vis.colors);
 
-    //==================END======================================================================================================================
+        // Initialize axes
+        vis.xAxis = d3.axisTop(vis.monthScale)
+            .tickValues(vis.config.monthLabels)
+            .tickSize(15)
+            .tickPadding(5);
 
-    // Initialize arc generator that we use to create the SVG path for the half circles. 
-    vis.arcGenerator = d3.arc()
-        .outerRadius(d => vis.radiusScale(d))
-        .innerRadius(0)
-        .startAngle(-Math.PI / 2)
-        .endAngle(Math.PI / 2);
+        vis.yAxis = d3.axisLeft(vis.yScale)
+            .tickSize(-vis.width)
+            .tickPadding(15)
+            .ticks(30)
+            .tickFormat(d3.format("d"));
 
-    // Define size of SVG drawing area
-    vis.svg = d3.select(vis.config.parentElement).append('svg')
-        .attr('width', vis.config.containerWidth)
-        .attr('height', vis.config.containerHeight);
+        //==================END======================================================================================================================
 
-    // Append group element that will contain our actual chart 
-    // and position it according to the given margin config
-    vis.chartArea = vis.svg.append('g')
-        .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+        // Initialize arc generator that we use to create the SVG path for the half circles.
+        vis.arcGenerator = d3.arc()
+            .outerRadius(d => vis.radiusScale(d))
+            .innerRadius(0)
+            .startAngle(-Math.PI / 2)
+            .endAngle(Math.PI / 2);
 
-    // Todo: Append axis groups
-    //===================BEGIN====================================================================================================
-    vis.xAxisG = vis.chartArea.append('g')
-        .attr('class', 'axis top-axis');
-    vis.yAxisG = vis.chartArea.append('g')
-        .attr('class', 'axis y-axis');
-    //===================END====================================================================================================
+        // Define size of SVG drawing area
+        vis.svg = d3.select(vis.config.parentElement).append('svg')
+            .attr('width', vis.config.containerWidth)
+            .attr('height', vis.config.containerHeight);
 
-    // Initialize clipping mask that covers the whole chart
-    vis.chartArea.append('defs')
-      .append('clipPath')
-        .attr('id', 'chart-mask')
-      .append('rect')
-        .attr('width', vis.width)
-        .attr('y', -vis.config.margin.top)
-        .attr('height', vis.config.containerHeight);
+        // Append group element that will contain our actual chart
+        // and position it according to the given margin config
+        vis.chartArea = vis.svg.append('g')
+            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
 
-    // Apply clipping mask to 'vis.chart' to clip semicircles at the very beginning and end of a year
-    vis.chart = vis.chartArea.append('g')
-        .attr('clip-path', 'url(#chart-mask)');
+        // Todo: Append axis groups
+        //===================BEGIN====================================================================================================
+        vis.xAxisG = vis.chartArea.append('g')
+            .attr('class', 'axis month-axis')
+            .attr('transform', d => `translate(-30,-30)`);
+        vis.yAxisG = vis.chartArea.append('g')
+            .attr('class', 'axis y-axis')
+            .attr('transform', d => `translate(0,5)`);
+        //===================END====================================================================================================
 
-    // Optional: other static elements
-    // ...
-    
-    vis.updateVis();
-  }
+        // Initialize clipping mask that covers the whole chart
+        vis.chartArea.append('defs')
+            .append('clipPath')
+            .attr('id', 'chart-mask')
+            .append('rect')
+            .attr('width', vis.width)
+            .attr('y', -vis.config.margin.top)
+            .attr('height', vis.config.containerHeight);
 
-  /**
-   * Prepare the data and scales before we render it.
-   */
-  updateVis() {
-    let vis = this;
+        // Apply clipping mask to 'vis.chart' to clip semicircles at the very beginning and end of a year
+        vis.chart = vis.chartArea.append('g')
+            .attr('clip-path', 'url(#chart-mask)');
 
-    // Group data per year (we get a nested array)
-    // [['1980', [array with values]], ['1981', [array with values]], ...]
-    vis.groupedData = d3.groups(vis.data, d => d.year);
+        // Optional: other static elements
+        // ...
 
-    // Sort states by total case numbers (if the option is selected by the user)
-    // Descending order
-    vis.groupedData.sort((a,b) => b[0] - a[0]);
+        vis.updateVis();
+    }
 
-    // Specificy accessor functions
-    vis.radiusValue = d => d.cost;
-    vis.yValue = d => d[0];
-    vis.xValue = d => d.dayOfYear;
-    vis.topValue = d => d.month;
+    /**
+     * Prepare the data and scales before we render it.
+     */
+    updateVis() {
+        let vis = this;
 
-    // Set the scale input domains
-    vis.radiusScale.domain(d3.extent(vis.data, vis.radiusValue));
-    vis.xScale.domain(d3.extent(vis.data, vis.xValue));
-    vis.yScale.domain(vis.groupedData.map(vis.yValue));
-    vis.topScale.domain([0, 11]);
+        // Group data per year (we get a nested array)
+        // [['1980', [array with values]], ['1981', [array with values]], ...]
+        vis.groupedData = d3.groups(vis.data, d => d.year);
 
-    console.log("vis.yScale(vis.yValue(1980)): ", vis.yScale("1980"));
-    console.log("vis.yScale(vis.yValue(2000)): ", vis.yScale(vis.yValue([2000])));
-    console.log("vis.yScale(vis.yValue(2017)): ", vis.yScale(vis.yValue([2017])));
-    console.log("vis.yScale(vis.yValue(1980)): ", vis.xScale(150));
-    console.log("vis.yScale(vis.yValue(2000)): ", vis.xScale(0));
-    console.log("vis.yScale(vis.yValue(2017)): ", vis.xScale(365));
-    // console.log(vis.yScale(vis.yValue(d)));
-    // console.log(vis.yScale(vis.yValue(d)));
+        vis.groupedData.forEach(d => {
+            d[2] = d3.max(d[1], k => k.cost);
+            d[3] = d[1].find(e => e.cost === d[2]).name;
+            d[4] = d[1].find(e => e.cost === d[2]).dayOfYear;
+        });
 
-    vis.renderVis();
-  }
+        // Specificy accessor functions
+        vis.yValue = d => d[0];
+        vis.yearValue = d => d.year;
+        vis.xValue = d => d.dayOfYear;
+        vis.monthValue = d => d.month;
 
-  /**
-   * Bind data to visual elements (enter-update-exit) and update axes
-   */
-  renderVis() {
-    let vis = this;
+        console.log(vis.groupedData);
 
-    // 1. Level: rows
-    const row = vis.chart.selectAll('.h-row')
-        .data(vis.groupedData, d=> d[0]);
+        vis.renderVis();
+    }
 
-    // Enter
-    const rowEnter = row.enter().append('g')
-        .attr('class', 'h-row');
+    /**
+     * Bind data to visual elements (enter-update-exit) and update axes
+     */
+    renderVis() {
+        let vis = this;
 
-    // Enter + update
-    rowEnter.merge(row)
-        .attr('transform', d => `translate(50,${vis.yScale(vis.yValue(d))})`);
+        // 1. Level: rows
+        const row = vis.chart.selectAll('.h-row')
+            .data(vis.groupedData, d => d[0]);
 
-    // Exit
-    rowEnter.exit().remove();
+        // Enter
+        const rowEnter = row.enter().append('g')
+            .attr('class', 'h-row');
 
-    // Append row label (y-axis)
-    rowEnter.append('text')
-        .attr('class', 'h-label')
-        .attr('text-anchor', 'end')
-        .attr('dy', '0.85em')
-        .attr('x', -8)
-        .text(vis.yValue);
+        // Enter + update
+        rowEnter.merge(row)
+            .attr('transform', d => `translate(0,${vis.yScale(vis.yValue(d)) + 5})`);
+
+        // Exit
+        row.exit().remove();
+
+        // Append row label (y-axis)
+        rowEnter.append('text')
+            .attr('class', 'h-label')
+            .attr('dy', 12)
+            .attr('text-anchor', 'middle')
+            .attr('x', d => vis.xScale(d[4]))
+            .attr('y', d => console.log(vis.yScale(d[0])))
+            .text(d => d[3]);
+
+        // 2. Level: columns
+
+        // 2a) Actual cells
+        const cell = row.merge(rowEnter).selectAll('.h-cell')
+            .data(d => d[1]);
+
+        // Enter
+        const cellEnter = cell.enter().append('path')
+            .attr('class', 'h-cell');
 
 
-    // 2. Level: columns
+        // Enter + update
+        cellEnter.merge(cell)
+            .attr('d', d => vis.arcGenerator(d.cost))
+            .attr('transform', d => `translate(${vis.xScale(vis.xValue(d))},0)`)
+            .attr('fill', d => vis.colorScale(d.category))
+            .attr("fill-opacity", "0.6")
+            .attr("stroke", "#333")
+            .attr("stroke-width", 0.3)
+            .on('mouseover', (event, d) => {
+                const value = (d.value === null) ? 'No data available' : Math.round(d.value * 100) / 100;
+                d3.select('#tooltip')
+                    .style('display', 'block')
+                    .style('left', (event.pageX + vis.config.tooltipPadding) + 'px')
+                    .style('top', (event.pageY + vis.config.tooltipPadding) + 'px')
+                    .html(`<div class='tooltip-title'>${d.name}</div>
+                           <div><strong>$${d.cost} billion</strong></div>`);
+            })
+            .on('mouseleave', () => {
+                d3.select('#tooltip').style('display', 'none');
+            });
 
-    // 2a) Actual cells
-    const cell = row.merge(rowEnter).selectAll('.h-cell')
-        .data(d => d[1]);
-
-    // Enter
-    const cellEnter = cell.enter().append('path')
-        .attr('class', 'h-cell');
-
-    // Enter + update
-    cellEnter.merge(cell)
-        .attr('d', d => vis.arcGenerator(d.cost))
-        .attr('transform', d => `translate(${vis.xScale(vis.xValue(d))},0)`);
-        //.attr('x', d => vis.xScale(vis.xValue(d)));
-        // .attr('fill', d => {
-        //   if (d.value === 0 || d.value === null) {
-        //     return '#fff';
-        //   } else {
-        //     return vis.colorScale(vis.colorValue(d));
-        //   }
-        // })
+        cell.exit().remove();
         // .on('mouseover', (event,d) => {
         //   const value = (d.value === null) ? 'No data available' : Math.round(d.value * 100) / 100;
         //   d3.select('#tooltip')
@@ -209,39 +223,40 @@ class Timeline {
         //   d3.select('#tooltip').style('display', 'none');
         // });
 
-    // // 2b) Diagonal lines for NA values
-    // const cellNa = row.merge(rowEnter).selectAll('.h-cell-na')
-    //     .data(d => d[1].filter(k => k.value === null));
-    //
-    // const cellNaEnter = cellNa.enter().append('line')
-    //     .attr('class', 'h-cell-na');
-    //
-    // cellNaEnter.merge(cellNa)
-    //     .attr('x1', d => vis.xScale(vis.xValue(d)))
-    //     .attr('x2', d => vis.xScale(vis.xValue(d)) + cellWidth)
-    //     .attr('y1', vis.yScale.bandwidth())
-    //     .attr('y2', 0);
-    //
-    // // Set the positions of the annotations
-    // const xVaccineIntroduced = vis.xScale(vis.config.vaccineIntroduced);
-    // vis.vaccineLine
-    //     .attr('x1', xVaccineIntroduced)
-    //     .attr('x2', xVaccineIntroduced)
-    //     .attr('y1', -5)
-    //     .attr('y2', vis.config.height);
-    //
-    // vis.vaccineLabel.attr('x', xVaccineIntroduced);
+        // // 2b) Diagonal lines for NA values
+        // const cellNa = row.merge(rowEnter).selectAll('.h-cell-na')
+        //     .data(d => d[1].filter(k => k.value === null));
+        //
+        // const cellNaEnter = cellNa.enter().append('line')
+        //     .attr('class', 'h-cell-na');
+        //
+        // cellNaEnter.merge(cellNa)
+        //     .attr('x1', d => vis.xScale(vis.xValue(d)))
+        //     .attr('x2', d => vis.xScale(vis.xValue(d)) + cellWidth)
+        //     .attr('y1', vis.yScale.bandwidth())
+        //     .attr('y2', 0);
+        //
+        // // Set the positions of the annotations
+        // const xVaccineIntroduced = vis.xScale(vis.config.vaccineIntroduced);
+        // vis.vaccineLine
+        //     .attr('x1', xVaccineIntroduced)
+        //     .attr('x2', xVaccineIntroduced)
+        //     .attr('y1', -5)
+        //     .attr('y2', vis.config.height);
+        //
+        // vis.vaccineLabel.attr('x', xVaccineIntroduced);
 
-    // Update axis
-    vis.xAxisG.call(vis.xAxis);
-    // Todo
-  }
+        // Update axis
+        vis.xAxisG.call(vis.xAxis).call(g => g.select('.domain').remove());
+        vis.yAxisG.call(vis.yAxis).call(g => g.select('.domain').remove());
+        // Todo
+    }
 
-  // renderLegend() {
-  //   let vis = this;
-  //
-  //   // Todo: Display the disaster category legend that also serves as an interactive filter.
-  //   // You can add the legend also to `index.html` instead and have your event listener in `main.js`.
-  // }
+    // renderLegend() {
+    //   let vis = this;
+    //
+    //   // Todo: Display the disaster category legend that also serves as an interactive filter.
+    //   // You can add the legend also to `index.html` instead and have your event listener in `main.js`.
+    // }
 
 }
